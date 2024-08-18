f6c175150ea17c3b689e2abbf51fe9a3864f15dc
Temies0 <putilinartem21@gmail.com>
2024-08-18 22:41:30 +0300

    fivetenth commit

--- src/main.tsx
+++ src/main.tsx
@@ -5,49 +5,59 @@ import './globals/styles/main.scss'
 import './globals/styles/reset.css'
 // import RoutesConfig from './RoutesConfig'
 /// pages
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Route, Router, Routes } from 'react-router-dom'
 import CreateWalletPage from './pages/first_entry/CreateWalletPage'
 import LoadingPage from './pages/loading/LoadingPage'
 import LogInPage from './pages/log_in/LogInPage'
 import MainPage from './pages/main/MainPage'
 import WalletIsCreatedPage from './pages/wallet_is_created/WalletIsCreatedPage'
 
-const router = createBrowserRouter([
-	{
-		path: '/test-app/',
-		element: <CreateWalletPage />,
-		children: [
-			{
-				path: '/test-app/',
-				element: <CreateWalletPage />,
-			},
-			{
-				path: '/test-app/created',
-				element: <WalletIsCreatedPage />,
-			},
-			{
-				path: '/test-app/login',
-				element: <LogInPage />,
-			},
-			{
-				path: '/test-app/main',
-				element: <MainPage />,
-			},
-			{
-				path: '/test-app/loading',
-				element: <LoadingPage />,
-			},
-		],
-	},
-])
+// const router = createBrowserRouter([
+// 	{
+// 		path: '/test-app/',
+// 		element: <CreateWalletPage />,
+// 		children: [
+// 			{
+// 				path: '/test-app/',
+// 				element: <CreateWalletPage />,
+// 			},
+// 			{
+// 				path: '/test-app/created',
+// 				element: <WalletIsCreatedPage />,
+// 			},
+// 			{
+// 				path: '/test-app/login',
+// 				element: <LogInPage />,
+// 			},
+// 			{
+// 				path: '/test-app/main',
+// 				element: <MainPage />,
+// 			},
+// 			{
+// 				path: '/test-app/loading',
+// 				element: <LoadingPage />,
+// 			},
+// 		],
+// 	},
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		{/* <RouterProvider router={router} /> */}
 		{/* <LogInPage /> */}
 		{/* <MainPage /> */}
 		{/* <CreateWalletPage /> */}
 		{/* <LoadingPage /> */}
 		{/* <WalletIsCreatedPage /> */}
+		<Router>
+			<Routes>
+				<Route path='/test-app/' element={<CreateWalletPage />} />
+				<Route path='/test-app/loading' element={<LoadingPage />} />
+				<Route path='/test-app/created' element={<WalletIsCreatedPage />} />
+				<Route path='/test-app/login' element={<LogInPage />} />
+				<Route path='/test-app/main' element={<MainPage />} />
+			</Routes>
+		</Router>
 	</React.StrictMode>
 )
