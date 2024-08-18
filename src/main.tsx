3ad6dc986ceb5deb3474e6d10ff369c591334da1
Temies0 <putilinartem21@gmail.com>
2024-08-18 23:10:14 +0300

    twentiesecond commit

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import './globals/styles/main.scss'
 import './globals/styles/reset.css'
 // import RoutesConfig from './RoutesConfig'
 /// pages
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-// import { Route, HashRouter as Routes } from 'react-router-dom'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Route, HashRouter as Routes } from 'react-router-dom'
 
 import CreateWalletPage from './pages/first_entry/CreateWalletPage'
 import LoadingPage from './pages/loading/LoadingPage'
@@ -14,49 +14,49 @@ import LogInPage from './pages/log_in/LogInPage'
 import MainPage from './pages/main/MainPage'
 import WalletIsCreatedPage from './pages/wallet_is_created/WalletIsCreatedPage'
 
-const router = createBrowserRouter([
-	{
-		path: '/test-app/',
-		element: <CreateWalletPage />,
-		children: [
-			{
-				path: '/test-app/',
-				element: <CreateWalletPage />,
-			},
-			{
-				path: '/test-app/created',
-				element: <WalletIsCreatedPage />,
-			},
-			{
-				path: '/test-app/login',
-				element: <LogInPage />,
-			},
-			{
-				path: '/test-app/main',
-				element: <MainPage />,
-			},
-			{
-				path: '/test-app/loading',
-				element: <LoadingPage />,
-			},
-		],
-	},
-])
+// const router = createBrowserRouter([
+// 	{
+// 		path: '/test-app/',
+// 		element: <CreateWalletPage />,
+// 		children: [
+// 			{
+// 				path: '/test-app/',
+// 				element: <CreateWalletPage />,
+// 			},
+// 			{
+// 				path: '/test-app/created',
+// 				element: <WalletIsCreatedPage />,
+// 			},
+// 			{
+// 				path: '/test-app/login',
+// 				element: <LogInPage />,
+// 			},
+// 			{
+// 				path: '/test-app/main',
+// 				element: <MainPage />,
+// 			},
+// 			{
+// 				path: '/test-app/loading',
+// 				element: <LoadingPage />,
+// 			},
+// 		],
+// 	},
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		{/* <RouterProvider router={router} /> */}
 		{/* <LogInPage /> */}
 		{/* <MainPage /> */}
 		{/* <CreateWalletPage /> */}
 		{/* <LoadingPage /> */}
 		{/* <WalletIsCreatedPage /> */}
-		{/* <Routes>
+		<Routes>
 			<Route path='/test-app/' element={<CreateWalletPage />} />
 			<Route path='/test-app/loading' element={<LoadingPage />} />
 			<Route path='/test-app/created' element={<WalletIsCreatedPage />} />
 			<Route path='/test-app/login' element={<LogInPage />} />
 			<Route path='/test-app/main' element={<MainPage />} />
-		</Routes> */}
+		</Routes>
 	</React.StrictMode>
 )
